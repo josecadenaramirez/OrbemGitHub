6304140912aea616bf1ca7f61b82a0720e574030
José Cadena <cadena@MacBook-Pro-de-Jose.local>
2017-01-23 12:41:07 -0600

    Subiendo a github

--- GitHub/ViewController.swift
+++ GitHub/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ya está en github!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
