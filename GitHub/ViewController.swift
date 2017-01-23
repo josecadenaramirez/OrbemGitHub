be4140e93ee30a0af46e2bde383f0ae2648748a3
José Cadena <cadena@MacBook-Pro-de-Jose.local>
2017-01-23 12:38:38 -0600

    Initial Commit

---
+++ GitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by José Cadena on 23/01/17.
+//  Copyright © 2017 GitHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
