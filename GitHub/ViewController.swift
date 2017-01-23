186a981744493056b57aec289c37507588daef56
José Cadena <cadena@MacBook-Pro-de-Jose.local>
2017-01-23 12:49:43 -0600

    rama

--- GitHub/ViewController.swift
+++ GitHub/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ya está en github!")
+        print("branch master")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
